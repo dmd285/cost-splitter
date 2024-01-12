cb3dd08c373f46ff9dab835d7ca5fed669fd7708
dominickdevincenzo <devindm101@gmail.com>
2024-01-12 13:40:58 -0500

    Final

--- src/components/FormSplitBill.jsx
+++ src/components/FormSplitBill.jsx
@@ -1,14 +1,20 @@
 import { useState } from "react";
 import Button from "./Button";
 
-export default function FormSplitBill({ selectedFriend }) {
+export default function FormSplitBill({ selectedFriend, onSplitBill }) {
   const [bill, setBill] = useState("");
   const [paidByUser, setPaidByUser] = useState("");
   const paidByFriend = bill ? bill - paidByUser : "";
   const [whoIsPaying, setWhoIsPaying] = useState("user");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!bill || !paidByUser) return;
+    onSplitBill(whoIsPaying === "user" ? paidByFriend : -paidByUser);
+  }
+
   return (
-    <form className="form-split-bill">
+    <form className="form-split-bill" onSubmit={handleSubmit}>
       <h2>Split a bill with {selectedFriend.name} </h2>
 
       <label>💳 Bill value</label>
@@ -37,7 +43,7 @@ export default function FormSplitBill({ selectedFriend }) {
         value={whoIsPaying}
         onChange={(e) => setWhoIsPaying(e.target.value)}
       >
-        <option value="user">you</option>
+        <option value="user">You</option>
         <option value="friend">{selectedFriend.name}</option>
       </select>
 
